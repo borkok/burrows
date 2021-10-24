b140725f29bf058325312e510731156ec67693ba
kinga <kinga.borkowska.srv@gmail.com>
2021-10-24 20:07:40 +0200

    second green

--- src/main/java/MoveToFrontHelper.java
+++ src/main/java/MoveToFrontHelper.java
@@ -1,5 +1,18 @@
 public class MoveToFrontHelper {
+    private final char[] letters = new char[256];
+
+    public MoveToFrontHelper() {
+        for (char i = 0; i < 256; i++) {
+            letters[i] = i;
+        }
+    }
+
     public char encode(char in) {
-        return in;
+        char oldCode = letters[in];
+        letters[in] = 0; //new code, put to front
+        for (char i = 0; i < oldCode; i++) {
+            letters[i]++; //move the ones that were before 'in' to the right
+        }
+        return oldCode;
     }
 }
--- src/test/java/MoveToFrontHelperTest.java
+++ src/test/java/MoveToFrontHelperTest.java
@@ -11,7 +11,12 @@ import static org.assertj.core.api.Assertions.assertThat;
 class MoveToFrontHelperTest {
     private static Stream<Arguments> params() {
         return Stream.of(
-                Arguments.of("A", new Character[]{0x41})
+                Arguments.of("A", new Character[]{0x41}),
+                Arguments.of("AA", new Character[]{0x41, 0x00})
+/*                Arguments.of("ABRACADABRA!", new Character[]{
+                        0x41, 0x42, 0x52, 0x02, 0x44, 0x01, 0x45, 0x01, 0x04, 0x04, 0x02, 0x26
+                })*/
+                
         );
     }
 
