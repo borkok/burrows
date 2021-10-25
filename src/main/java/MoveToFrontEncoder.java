6f0a226f3ccd3d2ed4d52542a41943a528b858da
kinga <kinga.borkowska.srv@gmail.com>
2021-10-25 08:28:39 +0200

    refactor encoder

--- src/main/java/MoveToFrontEncoder.java
+++ src/main/java/MoveToFrontEncoder.java
@@ -1,4 +1,5 @@
 public class MoveToFrontEncoder {
+    // index is code, value is letter
     private final char[] letters = new char[256];
 
     public MoveToFrontEncoder() {
@@ -8,12 +9,17 @@ public class MoveToFrontEncoder {
     }
 
     public char encode(char theLetter) {
-        char code = letters[theLetter];
-        letters[theLetter] = 0; //new code, put to front
-        for (char letter = 0; letter < 256; letter++) {
-            //move letters that were before 'theLetter' to the right
-            if (letters[letter] < code && letter != theLetter)  letters[letter]++;
-        }
+        char code = findCode(theLetter);
+        ArrayRotator.rotateRight(letters, code);
         return code;
+    }
+
+    private char findCode(char theLetter) {
+        if (letters[theLetter] == theLetter) return theLetter;
+        for (char i = 0; i < 256; i++) {
+            if (letters[i] == theLetter) return i;
+        }
+        //should never happen
+        throw new IllegalStateException();
     }
 }
